b4e9e3c0334da5f28faef70e531fab0e4505639f
unixource <unixource@tuta.io>
2025-07-13 16:19:13 +0300

    Some code cleanup & added CI

--- src/utils.rs
+++ src/utils.rs
@@ -16,17 +16,3 @@ pub fn add(a: Vec<String>, b: Vec<String>) -> Vec<String> {
     }
     out
 }
-
-pub fn utf8_slice(s: &str, start: usize, end: usize) -> Option<&str> {
-    let mut iter = s
-        .char_indices()
-        .map(|(pos, _)| pos)
-        .chain(Some(s.len()))
-        .skip(start)
-        .peekable();
-    let start_pos = *iter.peek()?;
-    for _ in start..end {
-        iter.next();
-    }
-    Some(&s[start_pos..*iter.peek()?])
-}
